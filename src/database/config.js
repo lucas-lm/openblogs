b5b37ae765d71b74f721a0f7181aea24fcbfb137
Lucas Miranda <lucas-m@outlook.com>
2020-04-19 15:31:29 -0300

    chore: added path to resolve sqlite location

--- src/database/config.js
+++ src/database/config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   production: {
     dialect: 'postgres',
@@ -5,25 +7,13 @@ module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
   },
   development: {
     dialect: 'sqlite',
-    storage: './src/database/database.sqlite3',
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
+    storage: path.resolve(__dirname, 'database.sqlite'),
   },
   test: {
     dialect: 'sqlite',
     storage: ':memory',
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
   },
 }
