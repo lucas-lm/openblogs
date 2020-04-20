4f2fe5b9acf010b350c63be1f1a634d2e4509f85
Lucas Miranda <lucas-m@outlook.com>
2020-04-19 22:54:01 -0300

    feat: added new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const allow = require('../middlewares/authorization')
 const router = express.Router()
 
+const UserController = require('../controllers/UserController')
+const TokenController = require('../controllers/TokenController')
+const ArticleController = require('../controllers/ArticleController')
+
 router.get('/', (req, res) => res.send('Welcome to openblogs api'))
+router.post('/register', UserController.create)
+router.post('/get_token', TokenController.create)
+router.get('/articles', ArticleController.index)
+router.post('/article', allow('authenticated'), ArticleController.create)
+
+// show some results
+router.get('/test', (req, res) => {
+  const { auth } = req
+  const a = auth ? '' : 'não'
+  return res.send(`Você ${a} está autenticado`)
+})
 
 module.exports = router
